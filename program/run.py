bea3c580c76e27120b4bfefb630542a119b5a72b
Flaminia <flaminia.cavallo@essencedigital.com>
2015-04-17 15:19:12 +0100

    Add import data to sqlite database

--- program/run.py
+++ program/run.py
@@ -1,3 +1,4 @@
+import sqlite3
 import xlrd
 
 class Report():
@@ -6,7 +7,7 @@ class Report():
         self.skip_rows = 5 
         
     def set_path(self):
-        self.path = '/home/flaminiacavallo/workspace2/getting_biddable_back_on_track/report.xlsx'
+        self.path = '../reports/report.xlsx'
         #input('Please paste the path to your report.')
         
     def import_into_dict(self):
@@ -22,6 +23,24 @@ class Report():
                 if row['Account'] != 'Total':
                     list_rows.append(row)
         return list_rows
+    
+    def upload_report(self, list_rows):
+        # Test
+        for r in list_rows:
+            headers = r.keys()
+            values = ['"{0}"'.format(str(r[key])) for key in headers]
+            print("INSERT INTO t ({0}) VALUES ({1});".format(','.join(headers), ','.join(['?' for _ in range(len(headers))])))
+            print("INSERT INTO t ({0}) VALUES ({1});".format(','.join(headers), ','.join(values)))
+        # End of test
+        
+        con = sqlite3.connect(":memory:")
+        cur = con.cursor()
+        
+        for r in list_rows:
+            headers = r.keys()
+            values = [r[key] for key in headers]
+            cur.executemany("INSERT INTO t ({0}) VALUES ({1});".format(','.join(headers), ','.join(['?' for _ in range(len(headers))])), to_db)
+            con.commit()
     
     def process_report(self, mappings):
         pass
@@ -56,5 +75,6 @@ if __name__ == '__main__':
     r = Report()
     r.set_path()
     list_rows = r.import_into_dict()
+    r.upload_report(list_rows)
     print('done')
     
