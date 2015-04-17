fb59585a003a089a7606a4112acca21b0bb47610
Indrek Sepp <indreksepp@L318UBLT.essence.internal.com>
2015-04-17 16:21:08 +0100

    added fetchReport function to reportDB class

--- program/reportDB.py
+++ program/reportDB.py
@@ -1,8 +1,11 @@
 import sqlite3
+import itertools
+import datetime as dt
 
 class reportDB():
 	def __init__(self):
-		conn = sqlite3.connect("../reports.db")
+		conn = sqlite3.connect("../reports.db",timeout=1)
+		conn.row_factory=sqlite3.Row
 		self.c = conn.cursor()
 		self.initTables()
 
@@ -18,3 +21,22 @@ class reportDB():
                      	Tier text, Product text, Network text )''')
 
 
+	def fetchReport(self):
+		resultsDict = []
+		CurrentWeek = dt.datetime.now().isocalendar()[1]
+		for row in self.c.execute('''SELECT * FROM inputData JOIN mapping USING (Account,Campaign) '''):
+			rowDict = dict(itertools.izip(row.keys(),row))
+			try:
+				date = dt.datetime.strptime(rowDict['Date'],"%d/%m/%Y")
+			except ValueError:
+				continue
+
+			rowDict['Quarter'] = 1+(date.month-1)/3
+			rowDict['Month'] = date.month
+			rowDict['Week'] = date.isocalendar()[1]
+			rowDict['CurrentWeek'] = CurrentWeek
+
+			resultsDict.append(rowDict)
+
+		return resultsDict
+			
